88554461e08c88966836a42ca9c22ba869da5a80
SaltyShaman <kristoffer6400@gmail.com>
2025-10-18 15:34:51 +0200

    init commit

---
+++ app.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Serve everything in public
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Serve index.html at root
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/pages/frontend/index.html'));
+});
+
+
+const PORT = Number(process.env.PORT) || 8080;
+app.listen(PORT, () => {
+    console.log('Server is running on port:', PORT); 
+});
